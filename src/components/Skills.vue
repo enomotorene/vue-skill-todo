a3588575acffa4ccf33f03e65af651d1bf371dbf
enomotorene <enomoto.rene@gmail.com>
2019-07-07 16:54:37 +0900

    initial commit

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,12 +3,20 @@
     <div class="holder">
 
       <form @submit.prevent="addSkill">
-      <input type="text" placeholder="Enter a skill you have.." v-model="skill">
-
+      <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+      
+      <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+      <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+      </transition>
    
       </form>
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ data.skill}}</li>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+        <li v-for="(data, index) in skills" :key='index'>
+          {{ data.skill}}
+          <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+          </li>
+        </transition-group>
       </ul>
 
       <p>These are the skills that you passes</p>
@@ -33,9 +41,19 @@ export default {
   },
   methods: {
     addSkill() {
-      this.skills.push({skill: this.skill})
-      this.skill = '';
+      this.$validator.validateAll().then((result) => {
+        if(result) {
+          this.skills.push({skill: this.skill})
+          this.skill = '';
+        } else {
+          //console.log("Not valid");
+        }
+      })
       
+      
+    },
+    remove(id){
+      this.skills.splice(id,1);
     }
   }
 }
@@ -43,6 +61,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1"; /*animate.css*/
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+
   .holder {
     background: #fff;
   }
@@ -81,4 +102,37 @@ export default {
     background-color: #323333;
     color: #687F7F;
   }
+
+  .alert {
+    background: #fdf2ca;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100%{
+      transform: scale(1);
+    }
+  }
+
+i {
+  float: right;
+  cursor: pointer;
+}
 </style>
